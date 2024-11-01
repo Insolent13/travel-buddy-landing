0b27d66c2a9ecb5a101fca7fb68bc31e093391d8
Insolent13 <frau.erlang1@gmail.com>
2024-11-01 04:14:23 +0300

    add styles for companions page, add scripts for accordeon in filter form

---
+++ .history/src/js/index_20241101030346.js
@@ -0,0 +1,10 @@
+const header = document.querySelector(".header")
+const toggle = document.querySelector(".toggle-button")
+const rateButton = document.querySelector(".rate-button")
+const modal = document.querySelector(".modal")
+const btnModalClose = document.querySelector(".modal__close")
+const companionsFilter = document.querySelectorAll(".companions-filter__fieldset")
+const locationFilter = document.querySelector(".location-filter")
+const locationFilterLands = document.querySelector(".location-filter-lands")
+const btnCloseLocationFilter = document.querySelector(".location-filter__button")
+const 
